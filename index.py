fcfd518b2b4376b534dbfc5a185684008f1b4664
florianblanchet <florianblanchet@orange.fr>
2017-10-16 21:57:31 +0200

    confidentialite

--- index.py
+++ index.py
@@ -13,12 +13,12 @@ from bs4 import BeautifulSoup
 #TOKEN de prod
 token = os.environ.get('FB_ACCESS_TOKEN')
 FB_VERIFY_TOKEN = (os.environ.get('FB_VERIFY_TOKEN'))
-if token==None:
+#if token==None:
     #TOKEN de test
-    token="EAAFo1IiXrQwBABjWTFk7ZA4XL2kmhFt6M0t0pTjJWSGRppsTWQOYI7Lylcub4899ZBpZBOHz3N4CfvABclqw7ZA5CNZB1JtfhtFEVShC8KP3ZB3GLqmc5RLtnBX3WGl1aMM0zYvm6DAxIvCeOXem1YsFqpcVsrp1pZAzpkeF0QeCAZDZD"
+ #   token="EAAFo1IiXrQwBABjWTFk7ZA4XL2kmhFt6M0t0pTjJWSGRppsTWQOYI7Lylcub4899ZBpZBOHz3N4CfvABclqw7ZA5CNZB1JtfhtFEVShC8KP3ZB3GLqmc5RLtnBX3WGl1aMM0zYvm6DAxIvCeOXem1YsFqpcVsrp1pZAzpkeF0QeCAZDZD"
 
-if FB_VERIFY_TOKEN==None:
-    FB_VERIFY_TOKEN = "test_token"
+#if FB_VERIFY_TOKEN==None:
+#    FB_VERIFY_TOKEN = "test_token"
 
 me = "1437816462930392"
 
